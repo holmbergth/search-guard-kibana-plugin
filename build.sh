7d14419c6ef72c610bfab4d1a488239b589ada18
floragunn <dev@search-guard.com>
2017-04-16 11:41:34 +0200

    update build scripts

--- build.sh
+++ build.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
-PLUGIN_NAME=searchguard-kibana
-PLUGIN_VERSION=5.3.0-2
+set -e
+###################################
+###################################
 KIBANA_VERSION=5.3.0
+PLUGIN_VERSION=$KIBANA_VERSION-2
+PLUGIN_NAME=searchguard-kibana
+###################################
+###################################
 KIBANA_NODE_JS_VERSION_URL="https://raw.githubusercontent.com/elastic/kibana/v$KIBANA_VERSION/.node-version"
 echo "Building $PLUGIN_NAME-$PLUGIN_VERSION.zip"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
-#cd $DIR/..
-#git clone https://github.com/elastic/kibana.git
-#cd "kibana"
-#git fetch
-#git checkout tags/v$KIBANA_VERSION
-#hash nvm 2>/dev/null || export NVM_DIR=~/.nvm; mkdir -p $NVM_DIR; . $(brew --prefix nvm)/nvm.sh
-
-echo "Nodejs $(curl -s $KIBANA_NODE_JS_VERSION_URL)"
-nvm install "$(curl -s $KIBANA_NODE_JS_VERSION_URL)"
-
-
+NODEVERSION="$(curl -s $KIBANA_NODE_JS_VERSION_URL)"
+echo "Nodejs version: $NODEVERSION"
+nvm install "$NODEVERSION"
 rm -rf build/
 rm -rf node_modules/
 #npm install --save hapi@16.0.1
@@ -36,18 +33,5 @@ cd $DIR
 mkdir -p releases/$PLUGIN_VERSION/
 cp build/$PLUGIN_NAME-$PLUGIN_VERSION.zip releases/$PLUGIN_VERSION/
 echo "Created releases/$PLUGIN_VERSION/$PLUGIN_NAME-$PLUGIN_VERSION.zip"
-md5sum build/$PLUGIN_NAME-$PLUGIN_VERSION.zip
-
-echo "Uploading build/$PLUGIN_NAME-$PLUGIN_VERSION.zip"
-cresponse=$(curl --write-out %{http_code} --silent --output uploadresult -X POST -F fileUpload=@build/$PLUGIN_NAME-$PLUGIN_VERSION.zip 'https://www.filestackapi.com/api/store/S3?key=Aa30JUUJZQ4i5UPcU5BUHz')
-response="$(echo "$cresponse" | cut -c1-3)"
-
-if ! [[ $response == "200" ]] ; then
-   echo "Upload failed with status $response"
-   exit 1
-fi
-
-echo "Upload response: $response"
-echo "Result upload ############"
-cat uploadresult
-echo "##########################"
+SHA256="$(sha256sum build/$PLUGIN_NAME-$PLUGIN_VERSION.zip)"
+echo "SHA256: $SHA256"
